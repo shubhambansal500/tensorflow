ea4fa651dad2703d29a3e423c1fa2b2dc8c3db85
shubham.bansal <shubham.bansal@siemens.com>
2018-07-11 18:38:36 +0530

    added code to use video and capture image

--- Face_Detection/face_detection.py
+++ Face_Detection/face_detection.py
@@ -2,12 +2,12 @@ import tensorflow as tf
 import numpy as np
 import facenet
 from align import detect_face
-import cv2
+from cv2 import *
 import argparse
 
 parser = argparse.ArgumentParser()
-parser.add_argument("--img1", type = str, required=True)
-parser.add_argument("--img2", type = str, required=True)
+parser.add_argument("--img1", type=str, required=True)
+# parser.add_argument("--img2", type=str, required=True)
 args = parser.parse_args()
 
 # some constants kept as default from facenet
@@ -31,6 +31,7 @@ embeddings = tf.get_default_graph().get_tensor_by_name("embeddings:0")
 phase_train_placeholder = tf.get_default_graph().get_tensor_by_name("phase_train:0")
 embedding_size = embeddings.get_shape()[1]
 
+
 def getFace(img):
     faces = []
     img_size = np.asarray(img.shape)[0:2]
@@ -45,19 +46,23 @@ def getFace(img):
                 bb[2] = np.minimum(det[2] + margin / 2, img_size[1])
                 bb[3] = np.minimum(det[3] + margin / 2, img_size[0])
                 cropped = img[bb[1]:bb[3], bb[0]:bb[2], :]
-                resized = cv2.resize(cropped, (input_image_size,input_image_size),interpolation=cv2.INTER_CUBIC)
+                resized = cv2.resize(cropped, (input_image_size, input_image_size), interpolation=cv2.INTER_CUBIC)
                 prewhitened = facenet.prewhiten(resized)
-                faces.append({'face':resized,'rect':[bb[0],bb[1],bb[2],bb[3]],'embedding':getEmbedding(prewhitened)})
+                faces.append(
+                    {'face': resized, 'rect': [bb[0], bb[1], bb[2], bb[3]], 'embedding': getEmbedding(prewhitened)})
     return faces
+
+
 def getEmbedding(resized):
-    reshaped = resized.reshape(-1,input_image_size,input_image_size,3)
+    reshaped = resized.reshape(-1, input_image_size, input_image_size, 3)
     feed_dict = {images_placeholder: reshaped, phase_train_placeholder: False}
     embedding = sess.run(embeddings, feed_dict=feed_dict)
     return embedding
 
-def compare2face(img1,img2):
+
+def compare2face(img1, videoImg):
     face1 = getFace(img1)
-    face2 = getFace(img2)
+    face2 = getFace(videoImg)
     if face1 and face2:
         # calculate Euclidean distance
         dist = np.sqrt(np.sum(np.square(np.subtract(face1[0]['embedding'], face2[0]['embedding']))))
@@ -65,9 +70,33 @@ def compare2face(img1,img2):
     return -1
 
 
+def capture_image():
+    cam = VideoCapture(0)
+    global videoimage
+    s, videoimage = cam.read()
+    if s:
+        namedWindow("Taking Image...")
+        imshow("Taking Image...", videoimage)
+        waitKey(0)
+        imwrite("./tmp/shubham.jpg", videoimage)
+        destroyWindow("cam-test")
+
+
+if __name__ == "__main__":
+    capture_image()
+    file_name = "/tmp/image.jpg"
+    model_file = "tf_files/retrained_graph.pb"
+    label_file = "tf_files/retrained_labels.txt"
+    input_height = 299
+    input_width = 299
+    input_mean = 0
+    input_std = 255
+    input_layer = 'Mul'
+    output_layer = "final_result"
+
 img1 = cv2.imread(args.img1)
-img2 = cv2.imread(args.img2)
-distance = compare2face(img1, img2)
-threshold = 1.10    # set yourself to meet your requirement
-print("distance = "+str(distance))
+videoImg = videoimage
+distance = compare2face(img1, videoImg)
+threshold = 1.10  # set yourself to meet your requirement
+print("distance = " + str(distance))
 print("Result = " + ("same person" if distance <= threshold else "not same person"))
